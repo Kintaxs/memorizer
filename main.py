16e1c8a4df8c103d6f5f091b3eff5f7f8ab8fdd1
kintaxxs <travis2381@gmail.com>
2018-01-18 02:30:07 +0800

    add_file

--- main.py
+++ main.py
@@ -6,21 +6,41 @@ import sqlite3
 import sys
 from sql.DictSql import DictSql
 
-def add(vol_str):
+def add(add_vol):
     
-    if all(ord(c) < 128 for c in vol_str) == True:
-        print('ENG: ', vol_str)
+    if all(ord(c) < 128 for c in add_vol) == True:
+        dic = DictSql()
+        dic.add(add_vol)
+        del dic
     else:
         print('Not ENG: ', vol_str)
 
 def add_file(vol_file):
 
-    print(vol_file)
+    dic = DictSql()
+    not_exist = list()
+
+    with open(vol_file) as fp:
+        for line in fp.readlines():
+            if dic.add(line.rstrip('\n')) == 0:
+                not_exist.append(line.rstrip('\n'))
+    del dic
+
+    with open('daily/manual.md', 'a') as fp:
+        for word in not_exist:
+            fp.write(word+"\n")
+
 
 def search(search_vol):
 
     dic = DictSql()
     dic.search(search_vol)
+    del dic
+
+def list_all():
+
+    dic = DictSql()
+    dic.list_all()
     del dic
 
 if __name__ == '__main__':
--- sql/DictSql.py
+++ sql/DictSql.py
@@ -1,28 +1,57 @@
 import sqlite3
+import time
 
 class DictSql():
     def __init__(self):
-        self.conn = sqlite3.connect('sql/db/langdao-ec.db')
-        self.c = self.conn.cursor()
+        self.lang_conn = sqlite3.connect('sql/db/langdao-ec.db')
+        self.lang_cursor = self.lang_conn.cursor()
+        self.my_conn = sqlite3.connect('sql/db/my_vol.db')
+        self.my_cursor = self.my_conn.cursor()
 
     def search(self, search_vol):
 
-        result = self.c.execute('SELECT EXISTS(SELECT * FROM vocabulary WHERE name = ?)', (search_vol, ))
+        result = self.lang_cursor.execute('SELECT EXISTS(SELECT * FROM vocabulary WHERE name = ?)', (search_vol, ))
 
         for i in result:
             exist_flag = i[0]
             break
 
         if exist_flag == 1:
-            result = self.c.execute('SELECT * FROM vocabulary WHERE name = ?', (search_vol, ))
+            result = self.lang_cursor.execute('SELECT * FROM vocabulary WHERE name = ?', (search_vol, ))
 
             for i in result:
-                print('index: ',i[0])
-                print('name: ',i[1])
-                print('meaning: ', i[2].decode())
+                print('')
+                print(i[1], '\n')
+                print(i[2].decode(), '\n')
+            return 1
         else:
-            print('the volcabulary is not exist!')
+            print('The volcabulary is not exist!')
+            return 0
+
+    def add(self, search_vol):
+
+        if self.search(search_vol) == 0:
+            return 0
+
+        self.my_cursor.execute('SELECT EXISTS(SELECT * FROM vocabulary WHERE name = ?)', (search_vol, ))
+
+        if(self.my_cursor.fetchone()[0] == 0):
+            self.my_cursor.execute('select count(*) from vocabulary')
+            count = self.my_cursor.fetchone()[0] + 1
+            self.my_cursor.execute("insert into vocabulary(id, name, date, forget) values(?, ?, ?, 1)", (count, search_vol, int(time.time())))
+            self.my_conn.commit()
+        else:
+            print('The DB already have the word.')
+
+        return 1
+
+    def list_all(self):
+                               
+        self.my_cursor.execute('select * from vocabulary')
+        results = self.my_cursor.fetchall()
+        for result in results:
+            print(result)
 
     def __del__(self):
-        print('del DictSql')
-        self.conn.close()
+        self.lang_conn.close()
+        self.my_conn.close()
